a967993b50dc9b78eed35735a96f6e6ff76492ee
Peter Huang <peterhonline@gmail.com>
2020-07-30 21:50:04 -0400

    FCC - Url shortener microservice - all tests passed

---
+++ README.md
@@ -0,0 +1,23 @@
+# API Project: URL Shortener Microservice for freeCodeCamp
+
+### Project Links:
+
+**Final Project:** [https://rogue-lively-weather.glitch.me](https://rogue-lively-weather.glitch.me)
+
+### User Stories
+
+1. I can POST a URL to `https://rogue-lively-weather.glitch.me/api/shorturl/new` and I will receive a shortened URL in the JSON response. Example : `{"original_url":"www.google.com","short_url":1}`
+2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`. _HINT_: to be sure that the submitted url points to a valid site you can use the function `dns.lookup(host, cb)` from the `dns` core module.
+3. When I visit the shortened URL, it will redirect me to my original link.
+
+#### Creation Example:
+
+POST https://rogue-lively-weather.glitch.me/api/shorturl/new - body (urlencoded) : url=https://www.google.com
+
+#### Usage:
+
+https://rogue-lively-weather.glitch.me/api/shorturl/f0
+
+#### Will redirect to:
+
+https://www.freecodecamp.org
---
+++ server.js
@@ -0,0 +1,123 @@
+"use strict";
+
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+
+var cors = require("cors");
+
+var app = express();
+
+// Basic Configuration
+var port = process.env.PORT || 3000;
+
+// Setup BodyParser
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup DNS lookup
+var dns = require("dns");
+
+// Setup MongoDB
+process.env.MONGO_URI = "yourMongoDbAuthentication";
+var db = mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Setup Schema
+const RedirectUrlSchema = new mongoose.Schema({
+  short_url: String,
+  original_url: String,
+});
+
+var RedirectUrl = mongoose.model("RedirectUrl", RedirectUrlSchema);
+
+/** this project needs a db !! **/
+
+// mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.listen(port, function () {
+  console.log("Node.js listening ...");
+});
+
+// API url shorterner
+app.post("/api/shorturl/new", (req, res, next) => {
+  const url = req.body.url;
+
+  const regUrlEx = /^https?:\/\/www\.[\w-]+\.[\w-/]+$/g;
+
+  // Invalid url
+  if (!regUrlEx.test(url)) {
+    console.log("Error " + url);
+
+    res.json({
+      error: "invalid URL",
+    });
+  } else {
+    // Count number of documents in collection
+    RedirectUrl.countDocuments({}, (err, count) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // Insert new document
+        const i = url.indexOf(".") + 1;
+
+        const d = new RedirectUrl({
+          short_url: url.substring(i, i + 1) + count,
+          original_url: url,
+        });
+
+        d.save((err, docs) => {
+          if (err) {
+            console.log("Insert error: " + err);
+          }
+        });
+
+        res.json({
+          original_url: url,
+          short_url: url.substring(i, i + 1) + count,
+        });
+      }
+    });
+  }
+});
+
+// API url shorterner - redirect based on short url
+app.get("/api/shorturl/:dataString", (req, res, next) => {
+  const shortUrl = req.params.dataString;
+
+  RedirectUrl.find({ short_url: shortUrl }, (err, results) => {
+    if (err) {
+      res.send("No such shorten url in found in our database.");
+    } else {
+      if (results.length > 0) {
+        res.redirect(301, results[0].original_url);
+      } else {
+        res.send("No such shorten url in found in our database.");
+      }
+    }
+  });
+});
+
+app.get("/api/shorturl/", (req, res, next) => {
+  res.json({
+    error: "Invalid short url",
+  });
+});
